f8dc190f28524caeac617a7a653497ebdd0a419c
LIU <joshliu7106@gmail.com>
2026-05-29 21:37:46 +0800

    新增MemberVO

---
+++ src/main/java/com/freemind/member/model/MemberVO.java
@@ -0,0 +1,129 @@
+package com.freemind.member.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class MemberVO implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer memberId;        // member_id => memberId
+	private String memberAccount;    // member_account => memberAccount
+	private String memberPassword;   // member_password => memberPassword
+	private Integer accountStatus;   // account_status => accountStatus
+	private String name;             
+	private String gender;
+	private String phoneNumber;      // phone_number => phoneNumber
+	private LocalDate birthday;
+	private String city;
+	private String dist;
+	private String address;
+	private LocalDateTime regisAt;   // regis_at => regisAt
+	private byte[] profilePic;       // profile_pic => profilePic
+	private String cardNumber;       // card_number => cardNumber
+	private String nickname;
+	private String email;
+	private String bankAccount;      // bank_account => bankAccount
+	
+	public Integer getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberAccount() {
+		return memberAccount;
+	}
+	public void setMemberAccount(String memberAccount) {
+		this.memberAccount = memberAccount;
+	}
+	public String getMemberPassword() {
+		return memberPassword;
+	}
+	public void setMemberPassword(String memberPassword) {
+		this.memberPassword = memberPassword;
+	}
+	public Integer getAccountStatus() {
+		return accountStatus;
+	}
+	public void setAccountStatus(Integer accountStatus) {
+		this.accountStatus = accountStatus;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDist() {
+		return dist;
+	}
+	public void setDist(String dist) {
+		this.dist = dist;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public LocalDateTime getRegisAt() {
+		return regisAt;
+	}
+	public void setRegisAt(LocalDateTime regisAt) {
+		this.regisAt = regisAt;
+	}
+	public byte[] getProfilePic() {
+		return profilePic;
+	}
+	public void setProfilePic(byte[] profilePic) {
+		this.profilePic = profilePic;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+}
